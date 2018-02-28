21f57251cc509314493c5f6c2e2adf207df69ca5
Veronica Kim <vero.a.kim@gmail.com>
2018-02-28 01:16:39 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+
+attr_accessor :backed_projects, :name, :backer
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+      @backed_projects << project
+
+end
+
+
+
+
+end
